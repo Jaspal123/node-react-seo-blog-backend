85c7da39f566e31e3c290e7322b2a8c4b5985b33
karan1907 <goldyking11@gmail.com>
2019-12-04 22:05:31 +0530

    Added signout functionality and added protected route middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 
-const {signup, signin} = require("../controllers/auth");
+const {signup, signin, signout, requireSignin} = require("../controllers/auth");
 
 //validators
 const {runValidation} = require("../validators");
@@ -9,5 +9,13 @@ const {userSignUpValidator, userSignInValidator} = require("../validators/auth")
 
 router.post("/signup", userSignUpValidator, runValidation, signup)
 router.post("/signin", userSignInValidator, runValidation, signin)
+router.get("/signout", signout)
+
+//test
+router.get("/secret", requireSignin, (req,res) => {
+    res.json({
+        message: "You have access to secret page"
+    })
+})
 
 module.exports = router
